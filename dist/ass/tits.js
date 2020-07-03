83d1206301eda7da51ca53b11e313a0488a418e5
mihail <mihail@3axis.io>
2020-07-03 14:12:14 +0500

    Please enter the commit message for your changes. Lines starting

--- dist/ass/tits.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cool = void 0;
-function cool() {
-    console.log("cool");
-}
-exports.cool = cool;
-//# sourceMappingURL=tits.js.map
